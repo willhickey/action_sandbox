1ef91f7991fdad38b3aa6cbbde3a629862115e3c
Will Hickey <will.hickey@solana.com>
2022-05-26 15:26:43 -0500

    Add line to myfile

--- ci/update-and-create-pr.sh
+++ ci/update-and-create-pr.sh
@@ -1,8 +1,10 @@
+echo "foo bar"
+echo \"foo bar\"
 git checkout -b branch1
 git status
 echo "another line\n" >> myfile
 git add .
 git commit -m "Add line to myfile"
 git push origin branch1
-gh pr create --head --title \"PR Title\" --body \"PR Body\"
-
+gh pr create --title=PRTitle --body PRBody
+gh pr create --title "The bug is fixed" --body "Everything works again"
